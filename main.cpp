ab49bbfa59c5f3be37af05cf59f8436a1b952a73
Felix El-Hoiydi <felix.elhoiydi@bluewin.ch>
2022-11-09 12:17:39 +0100

    Add get input to main.cpp

--- main.cpp
+++ main.cpp
@@ -17,22 +17,38 @@ const string NO_SOLUTION("No path from start to end cells");
 // prototype de la fonction d'affichage de message d'error
 void print_error(string message, bool with_cell = false, unsigned i = 0, unsigned j = 0);
 
-void initialize_maze(vector<vector<int>> &maze);
+void initialize_maze(vector<vector<int>> &maze, vector<int> input);
 void display_maze(vector<vector<int>> maze);
 
-int main(int argc, char **argv)
+vector<int> get_input();
+
+int main()
 {
+    vector<int> input = get_input();
+    for (auto elem : input)
+    {
+        cout << elem << " ";
+    }
+    cout << endl;
+
     vector<vector<int>> maze;
-    initialize_maze(maze);
+    initialize_maze(maze, input);
     display_maze(maze);
     return 0;
 }
 
-void initialize_maze(vector<vector<int>> &maze)
+void initialize_maze(vector<vector<int>> &maze, vector<int> input)
 {
-    int rows = 5;
-    int cols = 10;
+    int rows = input[0];
+    int cols = input[1];
 
+    if (rows < 2 || cols < 2)
+    {
+        print_error(TOO_SMALL);
+        exit(EXIT_FAILURE);
+    }
+
+    // init border
     for (int i = 0; i < rows; i++)
     {
         vector<int> v;
@@ -48,9 +64,27 @@ void initialize_maze(vector<vector<int>> &maze)
 
             v.push_back(value);
         }
-
         maze.push_back(v);
     }
+    /*
+     int beginx = input[2];
+    int beginy = input[3];
+    int endx = input[4];
+    int endy = input[5];
+
+    maze[beginx][beginy] = 3;
+    maze[endx][endy] = 4;
+
+    // init obstacle
+    for (int i = 7; i < 7 + input[6] * 2; i += 2)
+    {
+        int x = input[i];
+        int y = input[i + 1];
+
+        maze[x][y] = -1;
+    }
+    */
+    // init begin end
 }
 
 void display_maze(vector<vector<int>> maze)
@@ -62,6 +96,14 @@ void display_maze(vector<vector<int>> maze)
             if (cell < 0)
             {
                 cout << "#";
+            }
+            else if (cell == 3)
+            {
+                cout << "A";
+            }
+            else if (cell == 4)
+            {
+                cout << "B";
             }
             else
             {
@@ -84,4 +126,26 @@ void print_error(string message, bool with_cell, unsigned i, unsigned j)
     cout << endl;
 
     exit(0);
+}
+
+vector<int> get_input()
+{
+    vector<int> input;
+    int a;
+    for (int i = 0; i < 6; i++)
+    {
+
+        cin >> a;
+        input.push_back(a);
+    }
+    int nobstacle;
+    cin >> nobstacle;
+    input.push_back(nobstacle);
+    for (int i = 0; i < nobstacle; i++)
+    {
+        cin >> a;
+        input.push_back(a);
+    }
+
+    return input;
 }
